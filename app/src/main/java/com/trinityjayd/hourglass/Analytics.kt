09f36a2421c5886da88a9dacc348a86a2a674156
TrinityJayd <trinitytiratram@gmail.com>
2023-05-28 17:33:45 +0000

    added all necessary screen files

---
+++ app/src/main/java/com/trinityjayd/hourglass/Analytics.kt
@@ -0,0 +1,11 @@
+package com.trinityjayd.hourglass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Analytics : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_analytics)
+    }
+}
---
+++ app/src/main/java/com/trinityjayd/hourglass/ListEntries.kt
@@ -0,0 +1,11 @@
+package com.trinityjayd.hourglass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListEntries : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_entries)
+    }
+}
