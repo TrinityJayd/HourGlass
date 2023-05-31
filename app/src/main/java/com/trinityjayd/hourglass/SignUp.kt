9b6bb1b5b4b55c079d287d5bcf34e59ea189bd4b
TrinityJayd <trinitytiratram@gmail.com>
2023-05-31 15:35:22 +0000

    improved responsiveness

--- app/src/main/java/com/trinityjayd/hourglass/SignUp.kt
+++ app/src/main/java/com/trinityjayd/hourglass/SignUp.kt
@@ -2,10 +2,20 @@ package com.trinityjayd.hourglass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        //get sign up button
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+        //set on click listener
+        signUpButton.setOnClickListener {
+            //go to sign up page
+            setContentView(R.layout.activity_home)
+        }
+
     }
 }
