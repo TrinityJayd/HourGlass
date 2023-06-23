6f6dfc5e5ced13983b76ea32252b0b8e94d798d5
Trinity Jayd <trinitytiratram@gmail.com>
2023-06-23 10:16:42 +0200

    updated home page

--- app/src/main/java/com/trinityjayd/hourglass/Home.kt
+++ app/src/main/java/com/trinityjayd/hourglass/Home.kt
@@ -1,17 +1,18 @@
 package com.trinityjayd.hourglass
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
 class Home : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         //get newEntry layout
-        val newEntry = findViewById<LinearLayout>(R.id.newEntryButton)
+        val newEntry = findViewById<CardView>(R.id.newEntryButton)
         //set onClickListener
         newEntry.setOnClickListener {
             //start newEntry activity
@@ -19,7 +20,7 @@ class Home : AppCompatActivity() {
         }
 
         //get viewEntries layout
-        val yourEntries = findViewById<LinearLayout>(R.id.yourEntriesButton)
+        val yourEntries = findViewById<CardView>(R.id.yourEntriesButton)
         //set onClickListener
         yourEntries.setOnClickListener {
             //start viewEntries activity
@@ -27,7 +28,7 @@ class Home : AppCompatActivity() {
         }
 
         //get goals layout
-        val goals = findViewById<LinearLayout>(R.id.goalsButton)
+        val goals = findViewById<CardView>(R.id.goalsButton)
         //set onClickListener
         goals.setOnClickListener {
             //start goals activity
@@ -35,7 +36,7 @@ class Home : AppCompatActivity() {
         }
 
         //get analytics layout
-        val analytics = findViewById<LinearLayout>(R.id.analyticsButton)
+        val analytics = findViewById<CardView>(R.id.analyticsButton)
         //set onClickListener
         analytics.setOnClickListener {
             //start analytics activity
@@ -43,7 +44,7 @@ class Home : AppCompatActivity() {
         }
 
         //get timer layout
-        val timer = findViewById<LinearLayout>(R.id.timerLayout)
+        val timer = findViewById<CardView>(R.id.timerButton)
         //set onClickListener
         timer.setOnClickListener {
             //start timer activity
@@ -51,7 +52,7 @@ class Home : AppCompatActivity() {
         }
 
         //get settings layout
-        val settings = findViewById<LinearLayout>(R.id.settingsLayout)
+        val settings = findViewById<CardView>(R.id.settingsButton)
         //set onClickListener
         settings.setOnClickListener {
             //start settings activity
